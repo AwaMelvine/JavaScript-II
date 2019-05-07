5042df11038816dd890d74beaaedd67db8c87979
Melvin Khan-Awa <melvinkhan-awa@Melvins-MacBook-Pro.local>
2019-05-07 15:48:00 +0100

    Implement stretch challenge in closures.js file

--- assignments/closure.js
+++ assignments/closure.js
@@ -15,10 +15,17 @@ function manipulate(number) {
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let counter = 0;
+  function increment() {
+    counter = counter + 1;
+    console.log(counter);
+  }
+  return increment;
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+// Example usage:
+const newCounter = counter();
+newCounter(); // 1
+newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
